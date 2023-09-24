fc23b7f228f758adb4bae1c9d637e3b09cc0e4aa
Anidasr <141925881+Anidasr@users.noreply.github.com>
2023-09-24 14:42:55 +0700

    StageSelection

---
+++ Assets/Script/StageManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    public Button buttonStage2;
+    public Button buttonStage3;
+
+    void Start()
+    {
+        CheckStage();
+    }
+
+    public void CheckStage()
+    {
+        int statusStage2 = PlayerPrefs.GetInt("Stage2");
+        int statusStageBoss = PlayerPrefs.GetInt("StageBoss");
+
+        if (statusStage2 == 1)
+            buttonStage2.interactable = true;
+        else
+            buttonStage2.interactable = false;
+
+        if (statusStageBoss == 1)
+            buttonStage3.interactable = true;
+        else
+            buttonStage3.interactable = false;
+    }
+
+    //Stage2
+    //StageBoss
+    //1 == Unlock
+    //0 == Lock
+}
